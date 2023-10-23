047bd7cbd388389098c53544060a095acb0244da
GuyApooye <guy.montia@gmail.com>
2023-10-23 20:56:46 +0300

    fixed stuff idk

--- src/main/java/org/guyapooye/mtp/loaders/recipes/ShapedRecipes.java
+++ src/main/java/org/guyapooye/mtp/loaders/recipes/ShapedRecipes.java
@@ -7,5 +7,6 @@ public class ShapedRecipes {
     public static void init() {
         RecipeHandler.addShapedRecipe("test_ulv", MTItems.TEST_TIERED_ULV,"AAA","A A", "AAA",'A', Items.COPPER_INGOT);
         RecipeHandler.addShapedRecipe("test_lv", MTItems.TEST_TIERED_LV,"AAA","ABA", "AAA",'A', Items.IRON_INGOT, 'B', MTItems.TEST_TIERED_ULV);
+        RecipeHandler.addShapedRecipe("test_mv", MTItems.TEST_TIERED_LV,"AAA","ABA", "AAA",'A', Items.IRON_INGOT, 'B', MTItems.TEST_TIERED_ULV);
     }
 }
